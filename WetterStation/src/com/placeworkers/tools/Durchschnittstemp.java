d21eeccbb3156228baa0cd34ceb015ee33435026
Dominic Klein <33520412+4k1r4sf4t3games@users.noreply.github.com>
2018-03-05 10:12:58 +0100

    Add files via upload

--- WetterStation/src/com/placeworkers/tools/Durchschnittstemp.java
+++ WetterStation/src/com/placeworkers/tools/Durchschnittstemp.java
@@ -2,8 +2,7 @@ package com.placeworkers.tools;
 
 public class Durchschnittstemp {
 
-	int[] day = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-	int[] temperatur = { 12, 14, 9, 12, 15, 16, 15, 15, 11, 8, 13, 13, 15, 12 };
+	
 	public int[] mTemp = null;
 
 	public Durchschnittstemp(int[] temp) {
